6599bf125a76825863b705cccd237c4c4fa0b5ea
bussra <145451633+bussrra@users.noreply.github.com>
2025-05-10 01:11:59 +0300

    Add files via upload

---
+++ satranc-sure-TIMER-main/button.ornek.54/button.54/button.54/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace button._54
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBeyaz = new System.Windows.Forms.Button();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnSiyah = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBeyaz
+            // 
+            this.btnBeyaz.Location = new System.Drawing.Point(97, 32);
+            this.btnBeyaz.Name = "btnBeyaz";
+            this.btnBeyaz.Size = new System.Drawing.Size(75, 23);
+            this.btnBeyaz.TabIndex = 0;
+            this.btnBeyaz.Text = "Beyaz";
+            this.btnBeyaz.UseVisualStyleBackColor = true;
+            this.btnBeyaz.Click += new System.EventHandler(this.btnBeyaz_Click);
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(26, 104);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(75, 23);
+            this.btnBaslat.TabIndex = 1;
+            this.btnBaslat.Text = "BAŞLAT";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnSiyah
+            // 
+            this.btnSiyah.Location = new System.Drawing.Point(97, 179);
+            this.btnSiyah.Name = "btnSiyah";
+            this.btnSiyah.Size = new System.Drawing.Size(75, 23);
+            this.btnSiyah.TabIndex = 2;
+            this.btnSiyah.Text = "Siyah";
+            this.btnSiyah.UseVisualStyleBackColor = true;
+            this.btnSiyah.Click += new System.EventHandler(this.btnSiyah_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "100";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "KAZANAN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 404);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSiyah);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.btnBeyaz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBeyaz;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnSiyah;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ satranc-sure-TIMER-main/button.ornek.54/button.54/button.54/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button._54
+{
+    public partial class Form1 : Form
+    {
+        int sayacB = 100;
+        int sayacS = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayacB--;
+            
+            
+            if (sayacB <=0)
+            {
+                timer1.Stop();
+            }
+            label1.Text=sayacB.ToString();
+            if(sayacB <= 0)
+            {
+                label3.Text = "SİYAH KAZANDI";
+            }
+
+        }
+
+        private void btnBeyaz_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            
+            timer2.Start();
+           
+        }
+
+        private void btnSiyah_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            
+            timer1.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            sayacS--;
+            
+            if (sayacS <= 0)
+            {
+                timer2.Stop();
+            }
+            label2.Text=sayacS.ToString();
+            if(sayacS <= 0)
+            {
+                label3.Text = "BEYAZ KAZANDI";
+            }
+        }
+    }
+}
