84d0fcfdae886c185f78587c0d814f0fac1282db
bussra <145451633+bussrra@users.noreply.github.com>
2025-05-10 01:11:13 +0300

    Add files via upload

---
+++ dogum-gunune-kalan-gun-hesaplama-main/button.ornek.53/button.53/button.53/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button._53
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            DateTime kalan = DateTime.Now;
+            DateTime gun = dateTimePicker1.Value;
+
+            TimeSpan ts = gun-kalan;
+
+            label1.Text = "DOĞUM GÜNÜN : "+ gun.ToShortDateString();
+            label2.Text = "KALAN GÜN : " + ts.Days.ToString();
+
+
+        }
+    }
+}
